89d480e3e82c3683db7438802ccb84a2ca4cbcfb
sabercode <sabercode@vip.qq.com>
2019-02-21 14:50:32 +0800

    First commit

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author: 刘涛 <liutao1495@emao.com>
+ */
+
+namespace composerDemo;
+
+
+class SayHello
+{
+    public static function World()
+    {
+        return "Hello World!!!";
+    }
+
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author: 刘涛 <liutao1495@emao.com>
+ */
+require_once __DIR__.'/vendor/autoload.php';
+use composerDemo\SayHello;
+echo SayHello::World();
